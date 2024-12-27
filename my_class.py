67c4c0394914ef4608d9c9364187938c174be0f3
meyn-theone <kiril.kilikhevych@gmail.com>
2024-12-27 15:14:24 +0100

    Add files via upload

--- my_class.py
+++ my_class.py
@@ -18,13 +18,32 @@ class Tamagochi:
             "heal": self.heal,
         }    
     
+
+    def new_round(self):
+
+        self.hunger = max(0, min(10, self.hunger))              #min(1value, 2value)    if 2value>1value
+        self.happiness = max(0, min(10, self.happiness))          #it will return 1value  
+        self.health = max(0, min(10, self.health))              #max(1value, 2value) if 2value > 1value
+        self.tiredness = max(0, min(10, self.tiredness))           #it will return 2 value 
+        self.poop = max(0, min(10, self.poop))   
+
+        self.hunger += randint(0, 2)        
+        self.health -= randint(0, 1)         
+        self.tiredness += randint(0, 2)      
+        self.poop += randint(0, 1)          
+        
+        self.status()
+
+   
+
+
+
     def status(self):
         print(f"Name: {self.name}")
         print(f"Hunger: {self.hunger}")
         print(f"Happiness: {self.happiness}")
         print(f"Health: {self.health}")
         print(f"Tiredness: {self.tiredness}")
-        print(f"Alive: {self.alive}")
         print(f"Poops: {self.poop}")
         if self.alive == False:
             print("Your pet is dead, be more patient next time.")   
@@ -37,18 +56,36 @@ class Tamagochi:
         else:
             print(f"Invalid action. Please choose from {list(self.actions.keys())}")
 
+    
+    
+    
     def death(self):
-        if self.hunger >= 10 or self.happiness <= 0 or self.health <= 0 or self.poop >= 10 or self.tiredness >= 10:
+        if self.hunger >= 10:
+            print("U died of hunger")
             self.alive = False
+        elif self.happiness <= 0: 
+            print("U died of your mental state")
+            self.alive = False
+        elif self.health <= 0:  
+            print("U died of illness")
+            self.alive = False
+        elif self.poop >= 10: 
+            print("U died of untidines")
+            self.alive = False
+        elif self.tiredness >= 10:
+            print("U died of tiredness")
+            self.alive = False
+        else:
+            self.alive = True 
 
     def feed(self):
-        if self.alive and self.hunger >= 1:
+        if self.alive and self.hunger >= 0:
             self.happiness += 2
-            self.hunger -= 3
+            self.hunger -= 6
             self.poop += randint(0, 2)
         else:
             print("You died of hunger")
-        self.status()
+        self.new_round()
 
     def sleep(self):
         if self.alive and self.tiredness <= 9:
@@ -57,49 +94,41 @@ class Tamagochi:
             self.tiredness -= 6
         else:
             print("You died of tiredness")
-        self.status()
+        self.new_round()
 
     def play(self):
         if self.alive and self.happiness >= 1:
             self.happiness += 5
             self.tiredness += 3
+            self.hunger += 3
         else:
             print("You died of boredom")
-        self.status()
+        self.new_round()
 
     def clean(self):
         if self.alive and self.poop <= 9:
             self.poop = 0
         else:
             print("Too dirty, you die")
-        self.status()
+        self.new_round()
 
     def heal(self):
         if self.alive and self.health >= 1:
             self.health = 10
         else:
             print("You died of illness")
-        self.status()
+        self.new_round()
 
-    def new_round(self):
-        self.hunger += randint(0, 2) 
-        self.health -= randint(0, 1)
-        self.tiredness -= randint(0, 2)
-        self.hunger = min(self.hunger, 10)
-        self.health = max(0, self.health)
-        self.happiness = max(0, self.happiness)
-        self.tiredness = max(0, self.tiredness)
 
 myPet = Tamagochi("Amigo")
 
 while True:
+    action = input("Enter an action:").strip().lower()
+    myPet.perform_action(action)
+   
     if not myPet.alive:
         break
-    action = input("Enter an action: ").strip().lower()
-    myPet.perform_action(action)
-    myPet.new_round()
-
-   
+ 
    
 
         
